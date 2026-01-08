33e83e31a8e451117aa7c56dee25c7a5beebff4b
Juxnlx <juanluisbrisquez@gmail.com>
2026-01-08 10:26:05 +0100

    Ejercicio Hibernate...

---
+++ EjercicioJuanLuis/src/main/java/ProyectoHibernate/EjercicioJuanLuis/App.java
@@ -0,0 +1,13 @@
+package ProyectoHibernate.EjercicioJuanLuis;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
